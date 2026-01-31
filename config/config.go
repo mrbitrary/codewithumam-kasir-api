d4b9b7141f646c2bd54a218b9e5d79cf34caa890
mrbitrary <mrbitrary@LEGIONSL5.localdomain>
2026-02-01 01:25:10 +0900

    adjust to be supabase compatible

--- config/config.go
+++ config/config.go
@@ -8,12 +8,7 @@ type Config struct {
 }
 
 type PostgresConfig struct {
-	Host            string        `mapstructure:"HOST"`
-	Port            string        `mapstructure:"PORT"`
-	User            string        `mapstructure:"USER"`
-	Password        string        `mapstructure:"PASSWORD"`
-	DBName          string        `mapstructure:"DB_NAME"`
-	SSLMode         string        `mapstructure:"SSL_MODE"`
+	ConnString      string        `mapstructure:"CONN_STRING"`
 	MaxConns        int32         `mapstructure:"MAX_CONNS"`
 	MaxIdleConnTime time.Duration `mapstructure:"MAX_IDLE_CONN_TIME"`
 	PingTimeout     time.Duration `mapstructure:"PING_TIMEOUT"`
--- config/config_test.go
+++ config/config_test.go
@@ -11,12 +11,7 @@ func TestConfig(t *testing.T) {
 	cfg := Config{
 		Port: "8080",
 		Postgres: PostgresConfig{
-			Host:            "localhost",
-			Port:            "5432",
-			User:            "testuser",
-			Password:        "testpass",
-			DBName:          "testdb",
-			SSLMode:         "disable",
+			ConnString:      "postgres://testuser:testpass@localhost:5432/testdb?sslmode=disable",
 			MaxConns:        10,
 			MaxIdleConnTime: 5 * time.Minute,
 			PingTimeout:     3 * time.Second,
@@ -24,12 +19,7 @@ func TestConfig(t *testing.T) {
 	}
 
 	assert.Equal(t, "8080", cfg.Port)
-	assert.Equal(t, "localhost", cfg.Postgres.Host)
-	assert.Equal(t, "5432", cfg.Postgres.Port)
-	assert.Equal(t, "testuser", cfg.Postgres.User)
-	assert.Equal(t, "testpass", cfg.Postgres.Password)
-	assert.Equal(t, "testdb", cfg.Postgres.DBName)
-	assert.Equal(t, "disable", cfg.Postgres.SSLMode)
+	assert.Equal(t, "postgres://testuser:testpass@localhost:5432/testdb?sslmode=disable", cfg.Postgres.ConnString)
 	assert.Equal(t, int32(10), cfg.Postgres.MaxConns)
 	assert.Equal(t, 5*time.Minute, cfg.Postgres.MaxIdleConnTime)
 	assert.Equal(t, 3*time.Second, cfg.Postgres.PingTimeout)
@@ -37,23 +27,13 @@ func TestConfig(t *testing.T) {
 
 func TestPostgresConfig(t *testing.T) {
 	pgConfig := PostgresConfig{
-		Host:            "db.example.com",
-		Port:            "5433",
-		User:            "admin",
-		Password:        "secret",
-		DBName:          "production",
-		SSLMode:         "require",
+		ConnString:      "postgres://admin:secret@db.example.com:5433/production?sslmode=require",
 		MaxConns:        25,
 		MaxIdleConnTime: 10 * time.Minute,
 		PingTimeout:     5 * time.Second,
 	}
 
-	assert.NotEmpty(t, pgConfig.Host)
-	assert.NotEmpty(t, pgConfig.Port)
-	assert.NotEmpty(t, pgConfig.User)
-	assert.NotEmpty(t, pgConfig.Password)
-	assert.NotEmpty(t, pgConfig.DBName)
-	assert.NotEmpty(t, pgConfig.SSLMode)
+	assert.NotEmpty(t, pgConfig.ConnString)
 	assert.Greater(t, pgConfig.MaxConns, int32(0))
 	assert.Greater(t, pgConfig.MaxIdleConnTime, time.Duration(0))
 	assert.Greater(t, pgConfig.PingTimeout, time.Duration(0))
